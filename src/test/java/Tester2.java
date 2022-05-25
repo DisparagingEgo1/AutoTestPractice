6d82fa640eb4190a62398bffd62fdf11217aefe1
Ocean Oestreicher <ooestreicher.ewu.edu>
2022-05-25 10:27:32 -0700

    More Tests

---
+++ src/test/java/Tester2.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Tester2 {
+    @Test
+    @DisplayName("5-2==3")
+    public void subtractTest(){
+        assertEquals(7,Tested.subtract(5,2));
+    }
+}
