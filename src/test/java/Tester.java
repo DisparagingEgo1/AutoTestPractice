402280bd8cf69a7227f1e7ea394ca7c7f2d95fd4
Ocean Oestreicher <ooestreicher.ewu.edu>
2022-05-25 09:58:48 -0700

    Display Name

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Tester {
 
     @Test
+    @DisplayName("Does 3+4 = 7?")
     public void testAdd(){
         assertEquals(7, Tested.add(3, 4));
     }
